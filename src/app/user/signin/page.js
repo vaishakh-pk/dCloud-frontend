925091dbecb07176e64a15eea430c986812afefe
VK <vaishakh.aravind@gmail.com>
2023-04-28 00:20:25 +0530

    login page

--- src/app/user/signin/page.js
+++ src/app/user/signin/page.js
@@ -1,21 +1,21 @@
 import Head from 'next/head';
-import Image from 'next/image'
-export default function Home() {
+
+export default function Signin() {
     return (
         <div className="login-container">
             <Head>
                 <title>Sign in to your account</title>
             </Head>
             <div className="logo-container">
-                <img src="/dcloud.png" alt="dCloud" className="logo-img" />
+                <img src="/dcloud.png" alt="dCloud" className="logo-img"/>
 
             </div>
             <h2 className="title">Sign in to your account</h2>
 
-            <form action="#" method="POST" className="form">
+            <form action="src/app#" method="POST" className="form">
                 <div className="form-group">
                     <label htmlFor="email" className="label">Email address</label>
-                    <input id="email" name="email" type="email" autoComplete="email" required className="input" />
+                    <input id="email" name="email" type="email" autoComplete="email" required className="input"/>
                 </div>
 
                 <div className="form-group">
@@ -23,17 +23,18 @@ export default function Home() {
                         <label htmlFor="password" className="label">Password</label>
 
                     </div>
-                    <input id="password" name="password" type="password" autoComplete="current-password" required className="input" />
+                    <input id="password" name="password" type="password" autoComplete="current-password" required
+                           className="input"/>
                 </div>
                 <div className="text-sm">
-                    <a href="#" className="forgot-password">Forgot password?</a>
+                    <a href="src/app#" className="forgot-password">Forgot password?</a>
                 </div>
                 <div className="form-group">
                     <button type="submit" className="button">Sign in</button>
                 </div>
             </form>
 
-            <p className="not-a-member">Not a member? <a href="#" className="signup-link">Sign Up</a></p>
+            <p className="not-a-member">Not a member? <a href="/user/signup" className="signup-link">Sign Up</a></p>
         </div>
     )
 }
---
+++ src/app/user/signup/page.js
@@ -0,0 +1,43 @@
+import Head from 'next/head';
+
+export default function Signup() {
+    return (
+        <div className="login-container">
+            <Head>
+                <title>Sign up</title>
+            </Head>
+            <div className="logo-container">
+                <img src="/dcloud.png" alt="dCloud" className="logo-img"/>
+
+            </div>
+            <h2 className="title">Sign Up</h2>
+
+            <form action="src/app#" method="POST" className="form">
+                <div className="form-group">
+                    <label htmlFor="name" className="label">Name</label>
+                    <input id="name" name="name" type="text" required className="input"/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email" className="label">Email address</label>
+                    <input id="email" name="email" type="email" autoComplete="email" required className="input"/>
+                </div>
+
+                <div className="form-group">
+                    <div className="flex items-center justify-between">
+                        <label htmlFor="password" className="label">Password</label>
+
+                    </div>
+                    <input id="password" name="password" type="password" placeholder="Enter new password" autoComplete="current-password" required
+                           className="input"/>
+                    <input id="password" name="password" type="password" placeholder="Re-Enter password" autoComplete="current-password" required
+                           className="input"/>
+                </div>
+                <div className="form-group">
+                    <button type="submit" className="button">Sign Up</button>
+                </div>
+            </form>
+
+            <p className="already-a-member">Already a member? <a href="/user/signin" className="signin-link">Sign In</a></p>
+        </div>
+    )
+}
