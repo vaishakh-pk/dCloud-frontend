226abc71028cbbbde0bf455fe9b36abb68a5dd91
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-07-11 02:36:19 +0530

    changed fav page css to local css(module)

--- src/app/favourites/page.jsx
+++ src/app/favourites/page.jsx
@@ -1,60 +1,83 @@
 import React, {Fragment} from 'react';
-import './favourites.css';
-import LeftSide from '../../components/Navbar/Navbar';
+import styles from './favourites.module.css';
 import Navbar from "../../components/Navbar/Navbar";
 
 function FavoritesLayout() {
     return (
         <Fragment>
             <Navbar/>
-            <div className="container">
-                <div className="main-content">
+            <div className={styles.container}>
+                <div className={styles['main-content']}>
                     {/* Main content in the middle */}
-                    <div className="search-bar">
-                        <input type="text" className="search-input" placeholder="Search"/>
+                    <div className={styles['search-bar']}>
+                        <input type="text" className={styles['search-input']} placeholder="Search" />
                     </div>
 
-                    <div className="favorites">
-                        <p className="favorites-font">Favorites</p>
-                        <button className="favorites-button">
-                            <img src="camera.png" alt="Button Image"/>
-                            <div className="favorites-button-text">
-                                <p className="favorites-button-text-name">Pictures</p>
-                                <p className="favorites-button-text-number">100 files</p>
+                    <div className={styles.favorites}>
+                        <p className={styles['favorites-font']}>Favorites</p>
+                        <button className={styles['favorites-button']}>
+                            <img src="camera.png" alt="Button Image" />
+                            <div className={styles['favorites-button-text']}>
+                                <p className={styles['favorites-button-text-name']}>Pictures</p>
+                                <p className={styles['favorites-button-text-number']}>100 files</p>
                             </div>
                         </button>
                     </div>
 
-                    <div className="files">
-                        <p className="files-font">Files</p>
-                        <button className="files-buttons"><img src="picture.png" alt="Profile Photo"/><p
-                            className="filename">file_0001</p></button>
-                        <button className="files-buttons"><img src="picture.png" alt="Profile Photo"/><p
-                            className="filename">file_0001</p></button>
-                        <button className="files-buttons"><img src="picture.png" alt="Profile Photo"/><p
-                            className="filename">file_0001</p></button>
-                        <button className="files-buttons"><img src="picture.png" alt="Profile Photo"/><p
-                            className="filename">file_0001</p></button>
-                        <button className="files-buttons"><img src="picture.png" alt="Profile Photo"/><p
-                            className="filename">file_0001</p></button>
-                        <button className="files-buttons"><img src="picture.png" alt="Profile Photo"/><p
-                            className="filename">file_0001</p></button>
+                    <div className={styles.files}>
+                        <p className={styles['files-font']}>Files</p>
+                        <button className={styles['files-buttons']}>
+                            <img src="picture.png" alt="Profile Photo" />
+                            <p className={styles.filename}>file_0001</p>
+                        </button>
+                        <button className={styles['files-buttons']}>
+                            <img src="picture.png" alt="Profile Photo" />
+                            <p className={styles.filename}>file_0001</p>
+                        </button>
+                        <button className={styles['files-buttons']}>
+                            <img src="picture.png" alt="Profile Photo" />
+                            <p className={styles.filename}>file_0001</p>
+                        </button>
+                        <button className={styles['files-buttons']}>
+                            <img src="picture.png" alt="Profile Photo" />
+                            <p className={styles.filename}>file_0001</p>
+                        </button>
+                        <button className={styles['files-buttons']}>
+                            <img src="picture.png" alt="Profile Photo" />
+                            <p className={styles.filename}>file_0001</p>
+                        </button>
+                        <button className={styles['files-buttons']}>
+                            <img src="picture.png" alt="Profile Photo" />
+                            <p className={styles.filename}>file_0001</p>
+                        </button>
                     </div>
 
-                    <div className="files">
-                        <p className="files-font"></p>
-                        <button className="files-buttons"><img src="picture.png" alt="Profile Photo"/><p
-                            className="filename">file_0001</p></button>
-                        <button className="files-buttons"><img src="picture.png" alt="Profile Photo"/><p
-                            className="filename">file_0001</p></button>
-                        <button className="files-buttons"><img src="picture.png" alt="Profile Photo"/><p
-                            className="filename">file_0001</p></button>
-                        <button className="files-buttons"><img src="picture.png" alt="Profile Photo"/><p
-                            className="filename">file_0001</p></button>
-                        <button className="files-buttons"><img src="picture.png" alt="Profile Photo"/><p
-                            className="filename">file_0001</p></button>
-                        <button className="files-buttons"><img src="picture.png" alt="Profile Photo"/><p
-                            className="filename">file_0001</p></button>
+                    <div className={styles.files}>
+                        <p className={styles['files-font']}></p>
+                        <button className={styles['files-buttons']}>
+                            <img src="picture.png" alt="Profile Photo" />
+                            <p className={styles.filename}>file_0001</p>
+                        </button>
+                        <button className={styles['files-buttons']}>
+                            <img src="picture.png" alt="Profile Photo" />
+                            <p className={styles.filename}>file_0001</p>
+                        </button>
+                        <button className={styles['files-buttons']}>
+                            <img src="picture.png" alt="Profile Photo" />
+                            <p className={styles.filename}>file_0001</p>
+                        </button>
+                        <button className={styles['files-buttons']}>
+                            <img src="picture.png" alt="Profile Photo" />
+                            <p className={styles.filename}>file_0001</p>
+                        </button>
+                        <button className={styles['files-buttons']}>
+                            <img src="picture.png" alt="Profile Photo" />
+                            <p className={styles.filename}>file_0001</p>
+                        </button>
+                        <button className={styles['files-buttons']}>
+                            <img src="picture.png" alt="Profile Photo" />
+                            <p className={styles.filename}>file_0001</p>
+                        </button>
                     </div>
                 </div>
             </div>
