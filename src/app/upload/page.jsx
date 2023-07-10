5d5fcf7621c2973b3442a9b527cfa8f0fe4041a2
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-07-11 02:45:46 +0530

    changed upload page css to local css(module)

--- src/app/upload/page.jsx
+++ src/app/upload/page.jsx
@@ -1,5 +1,5 @@
 import React, {Fragment} from 'react';
-import './upload.css';
+import styles from './upload.module.css';
 import Navbar from "@/components/Navbar/Navbar";
 
 
@@ -7,69 +7,64 @@ function UploadPageLayout() {
     return (
         <Fragment>
             <Navbar/>
-            <div className="container">
-                <div className="main-content">
-                    <div className="upload-files-box">
-                        <h2 className="uploadfiles">Upload Files</h2>
-                        <div className="grey-box">
-                            <img src="upload.png" alt="Upload Icon" className="upload-icon"/>
-                            <h2 className="upload-heading">Drag or drop your files here</h2>
-                            <div className="drag-drop-text">or</div>
-                            <button className="choose-button">Choose Files</button>
+            <div className={styles.container}>
+                <div className={styles['main-content']}>
+                    <div className={styles['upload-files-box']}>
+                        <h2 className={styles.uploadfiles}>Upload Files</h2>
+                        <div className={styles['grey-box']}>
+                            <img src="upload.png" alt="Upload Icon" className={styles['upload-icon']}/>
+                            <h2 className={styles['upload-heading']}>Drag or drop your files here</h2>
+                            <div className={styles['drag-drop-text']}>or</div>
+                            <button className={styles['choose-button']}>Choose Files</button>
                         </div>
                     </div>
-
-                    <div className="upload-progress-box">
-                        <h2 className="upload-progress-heading">
-                            <div className="image-container">
+                    <div className={styles['upload-progress-box']}>
+                        <h2 className={styles['upload-progress-heading']}>
+                            <div className={styles['image-container']}>
                                 <img src="progress.avif" alt="Upload Progress Image"/>
                             </div>
                             Upload Progress
                         </h2>
-                        <div className="progress-container">
-                            <div className="file-progress">
-                                <div className="grey-box">
-                                    <div className="file-info">
-                                        <div className="file-icon">
+                        <div className={styles['progress-container']}>
+                            <div className={styles['file-progress']}>
+                                <div className={styles['grey-box']}>
+                                    <div className={styles['file-info']}>
+                                        <div className={styles['file-icon']}>
                                             <img src="camera.png" alt="File Icon"/>
                                         </div>
-                                        <div className="file-details">
-                                            <div className="file-name">example_file1.txt</div>
+                                        <div className={styles['file-details']}>
+                                            <div className={styles['file-name']}>example_file1.txt</div>
                                         </div>
                                     </div>
-                                    <div className="rectanglestorage">
-                                        <div className="storage-meter">
+                                    <div className={styles['rectanglestorage']}>
+                                        <div className={styles['storage-meter']}>
                                             <progress value="60" max="100"></progress>
                                             <span>50%</span>
-                                            <img src="cross.png" alt="tick or cross" className="end-image"/>
+                                            <img src="cross.png" alt="tick or cross" className={styles['end-image']}/>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <div className="file-progress">
-                                <div className="grey-box">
-                                    <div className="file-info">
-                                        <div className="file-icon">
+                            <div className={styles['file-progress']}>
+                                <div className={styles['grey-box']}>
+                                    <div className={styles['file-info']}>
+                                        <div className={styles['file-icon']}>
                                             <img src="camera.png" alt="File Icon"/>
                                         </div>
-                                        <div className="file-details">
-                                            <div className="file-name">example_file2.txt</div>
+                                        <div className={styles['file-details']}>
+                                            <div className={styles['file-name']}>example_file2.txt</div>
                                         </div>
                                     </div>
-                                    <div className="rectanglestorage">
-                                        <div className="storage-meter">
+                                    <div className={styles['rectanglestorage']}>
+                                        <div className={styles['storage-meter']}>
                                             <progress value="100" max="100"></progress>
                                             <span>100%</span>
-                                            <img src="tick.png" alt="tick or cross" className="end-image"/>
+                                            <img src="tick.png" alt="tick or cross" className={styles['end-image']}/>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-
-                    <div className="right-side">
-                        {/* Content for the right side (if any) */}
                     </div>
                 </div>
             </div>
