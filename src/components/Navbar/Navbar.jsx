60ada40e33e2726da2d75f8657f79b975a2267fd
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-07-11 02:48:32 +0530

    changed navbar page css to local css(module) and some error fixes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,24 +2,24 @@
 import React from 'react';
 import Link from "next/link";
 import { useRouter } from 'next/navigation';
-import './Navbar.css';
+import styles from './Navbar.module.css';
 
 function Navbar() {
     const router = useRouter();
     return (
-        <div className="left-side">
-            <div className="profile-photo">
+        <div className={styles['left-side']}>
+            <div className={styles['profile-photo']}>
                 <img src="/avatar.png" alt="Profile Photo" />
             </div>
             <div>
-                <button onClick={() => router.push("/mycloud")} className="button1 home">My Cloud</button>
-                <button onClick={() => router.push("/shared")} className="button1 home">Shared Files</button>
-                <button onClick={() => router.push("/favourites")} className="button1 home">Favorites</button>
-                <button onClick={() => router.push("/upload")} className="button1 home">Upload Files</button>
+                <button onClick={() => router.push('/mycloud')} className={`${styles.button1} ${styles.home}`}>My Cloud</button>
+                <button onClick={() => router.push('/shared')} className={`${styles.button1} ${styles.home}`}>Shared Files</button>
+                <button onClick={() => router.push('/favourites')} className={`${styles.button1} ${styles.home}`}>Favorites</button>
+                <button onClick={() => router.push('/upload')} className={`${styles.button1} ${styles.home}`}>Upload Files</button>
             </div>
-            <div className="leftbuttonbottom">
-                <button className="button2">Settings</button>
-                <button className="button2">Log out</button>
+            <div className={styles.leftbuttonbottom}>
+                <button className={styles.button2}>Settings</button>
+                <button className={styles.button2}>Log out</button>
             </div>
         </div>
     );
