7e56dcd8bd94a2e127809b70e9e3279541bf7af0
VK <vaishakh.aravind@gmail.com>
2023-07-23 01:32:51 +0530

    section wise pages

---
+++ src/app/mycloud/audio/page.jsx
@@ -0,0 +1,87 @@
+import React, {Fragment} from 'react';
+import styles from './audio.module.css';
+import Navbar from "@/components/Navbar/Navbar";
+
+function FavoritesLayout() {
+    return (
+        <Fragment>
+            <Navbar/>
+            <div className={styles.container}>
+                <div className={styles['main-content']}>
+                    {/* Main content in the middle */}
+                    <div className={styles['search-bar']}>
+                        <input type="text" className={styles['search-input']} placeholder="Search" />
+                    </div>
+
+                    <div className={styles.favorites}>
+                        <button className={styles['favorites-button']}>
+                            <img src="/mycloud/audio.png" alt="Button Image" />
+                            <div className={styles['favorites-button-text']}>
+                                <p className={styles['favorites-button-text-name']}>Audio</p>
+                                <p className={styles['favorites-button-text-number']}>100 files</p>
+                            </div>
+                        </button>
+                    </div>
+
+                    <div className={styles.files}>
+                        <p className={styles['files-font']}>Files</p>
+                        <button className={styles['files-buttons']}>
+                            <img src="/mycloud/audio.png" alt="Profile Photo" />
+                            <p className={styles.filename}>file_0001</p>
+                        </button>
+                        <button className={styles['files-buttons']}>
+                            <img src="/mycloud/audio.png" alt="Profile Photo" />
+                            <p className={styles.filename}>file_0001</p>
+                        </button>
+                        <button className={styles['files-buttons']}>
+                            <img src="/mycloud/audio.png" alt="Profile Photo" />
+                            <p className={styles.filename}>file_0001</p>
+                        </button>
+                        <button className={styles['files-buttons']}>
+                            <img src="/mycloud/audio.png" alt="Profile Photo" />
+                            <p className={styles.filename}>file_0001</p>
+                        </button>
+                        <button className={styles['files-buttons']}>
+                            <img src="/mycloud/audio.png" alt="Profile Photo" />
+                            <p className={styles.filename}>file_0001</p>
+                        </button>
+                        <button className={styles['files-buttons']}>
+                            <img src="/mycloud/audio.png" alt="Profile Photo" />
+                            <p className={styles.filename}>file_0001</p>
+                        </button>
+                    </div>
+
+                    <div className={styles.files}>
+                        <p className={styles['files-font']}></p>
+                        <button className={styles['files-buttons']}>
+                            <img src="/mycloud/audio.png" alt="Profile Photo" />
+                            <p className={styles.filename}>file_0001</p>
+                        </button>
+                        <button className={styles['files-buttons']}>
+                            <img src="/mycloud/audio.png" alt="Profile Photo" />
+                            <p className={styles.filename}>file_0001</p>
+                        </button>
+                        <button className={styles['files-buttons']}>
+                            <img src="/mycloud/audio.png" alt="Profile Photo" />
+                            <p className={styles.filename}>file_0001</p>
+                        </button>
+                        <button className={styles['files-buttons']}>
+                            <img src="/mycloud/audio.png" alt="Profile Photo" />
+                            <p className={styles.filename}>file_0001</p>
+                        </button>
+                        <button className={styles['files-buttons']}>
+                            <img src="/mycloud/audio.png" alt="Profile Photo" />
+                            <p className={styles.filename}>file_0001</p>
+                        </button>
+                        <button className={styles['files-buttons']}>
+                            <img src="/mycloud/audio.png" alt="Profile Photo" />
+                            <p className={styles.filename}>file_0001</p>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </Fragment>
+    );
+}
+
+export default FavoritesLayout;
